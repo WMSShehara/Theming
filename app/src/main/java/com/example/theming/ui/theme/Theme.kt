5a9aa761f4c3b28292ca451a96fa301b41f7e267
Shehara <t2wasu00@students.oamk.fi>
2024-02-01 15:57:08 +0200

    changed color theme

--- app/src/main/java/com/example/theming/ui/theme/Theme.kt
+++ app/src/main/java/com/example/theming/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -43,29 +44,25 @@ private val LightColorPalette = lightColorScheme(
 fun ThemingTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    //dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorPalette
-        else -> LightColorPalette
+    val colors = if (!darkTheme) {
+        LightColorPalette
+    } else {
+        DarkColorPalette
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colors.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
     MaterialTheme(
-        colorScheme = LightColorPalette,
+        colorScheme = colors,
         typography = Typography,
         content = content,
         shapes = shapes
